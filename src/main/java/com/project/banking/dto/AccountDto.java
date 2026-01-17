d63ce96c5ddaf7265b8451c465416c8372d6caee
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-01-17 11:17:08 +0200

    Banking_App

--- src/main/java/com/project/banking/dto/AccountDto.java
+++ src/main/java/com/project/banking/dto/AccountDto.java
@@ -1,12 +1,27 @@
 package com.project.banking.dto;
 
-import lombok.*;
 
-@Data
-@AllArgsConstructor
 public class AccountDto {
 
     private Long id;
     private String accountHolderName;
     private double balance;
+
+    public AccountDto(Long id, String accountHolderName, double balance) {
+        this.id = id;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
 }
