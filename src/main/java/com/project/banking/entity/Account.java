5ce6ca834035034624ee2c8fc1bc43aa67847b4f
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2026-01-17 11:17:00 +0200

    Banking_App

--- src/main/java/com/project/banking/entity/Account.java
+++ src/main/java/com/project/banking/entity/Account.java
@@ -1,15 +1,10 @@
 package com.project.banking.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+
 @NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "accounts")
 public class Account {
@@ -22,4 +17,34 @@ public class Account {
     private String accountHolderName;
 
     private double balance;
+
+    public Account(Long id, String accountHolderName, double balance) {
+        this.id = id;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
